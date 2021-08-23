a960a2fa8de38af009b39514d1732142ee9d9782
nooobcoder <suryashi2013@gmail.com>
2021-08-23 18:00:50 +0000

    fixes

--- deno-webdev-book/section-two-web-application/src/web/index.ts
+++ deno-webdev-book/section-two-web-application/src/web/index.ts
@@ -1,17 +1,17 @@
-import { Application, Router } from "../deps.ts"
-import { MuseumController } from "../museums/index.ts"
+import { Application, Router } from "../deps.ts";
+import { MuseumController } from "../museums/index.ts";
 
 interface CreateServerDependencies {
-  configuration: { PORT: number }
-  museum: MuseumController
+  configuration: { PORT: number };
+  museum: MuseumController;
 }
 
-const routerController = ({ get }: Router, museum: MuseumController) => {
-  get(
+const routerController = (router: Router, museum: MuseumController) => {
+  router.get(
     "/museums",
     async ({ response }) => (response.body = { museums: await museum.getAll() })
-  )
-}
+  );
+};
 
 export async function createServer({
   configuration: { PORT = 3000 },
@@ -20,34 +20,34 @@ export async function createServer({
   const networkRequest: Deno.PermissionDescriptor = {
     name: "net",
     host: `0.0.0.0:${PORT}`,
-  }
-  await Deno.permissions.request(networkRequest)
+  };
+  await Deno.permissions.request(networkRequest);
   if ((await Deno.permissions.query(networkRequest)).state === "granted") {
-    const app = new Application()
+    const app = new Application();
     app.use(({ response }) => {
-      response.body = "Hello World"
-    })
+      response.body = "Hello World";
+    });
 
-    const apiRouter = new Router({ prefix: "/api" })
-    routerController(apiRouter, museum)
+    const apiRouter = new Router({ prefix: "/api" });
+    routerController(apiRouter, museum);
 
     app.addEventListener("listen", ({ hostname }) => {
       console.log(
         `[ APPLICATION RUNNING AT http://${hostname || "localhost"}:${PORT}]`
-      )
-    })
+      );
+    });
     app.addEventListener("error", ({ message }) =>
       console.log(`An Error occured: ${message}`)
-    )
+    );
 
     // Invoking the router middlewares
-    app.use(apiRouter.routes())
-    app.use(apiRouter.allowedMethods())
+    app.use(apiRouter.routes());
+    app.use(apiRouter.allowedMethods());
 
     // This shall always remain at the bottom
-    await app.listen({ hostname: "0.0.0.0", port: PORT })
+    await app.listen({ hostname: "0.0.0.0", port: PORT });
   } else {
-    console.error("[ NETWORK ACCESS WAS NOT ALLOWED ]")
-    Deno.exit()
+    console.error("[ NETWORK ACCESS WAS NOT ALLOWED ]");
+    Deno.exit();
   }
 }

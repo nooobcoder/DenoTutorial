e936151dc67a3ee733ca227b5ca0f6717c5fd6b4
nooobcoder <suryashi2013@gmail.com>
2021-08-23 04:29:34 +0000

    Added a fixture for the database

--- deno-webdev-book/section-two-web-application/src/index.ts
+++ deno-webdev-book/section-two-web-application/src/index.ts
@@ -12,3 +12,13 @@ const museumController: MuseumController = new MuseumController({
 
 // Top level await is allowed in Deno
 console.log(await museumController.getAll());
+
+// Adding a fixture to our "database" (JS Map being used as a database)
+museumRepository.storage.set("1fbdd2a9-1b97-46e0-b450-62819e5772ff", {
+  id: "1fbdd2a9-1b97-46e0-b450-62819e5772ff",
+  name: "The Louvre",
+  description:
+    "The world's largest art museum     and a historic monument in Paris, France.",
+  location: { lat: "48.860294", lng: "2.33862" },
+});
+console.log(await museumRepository.getAll());
